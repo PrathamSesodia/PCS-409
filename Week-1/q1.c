e7411f9913106318bb42b94d3376bc3efd345c70
PrathamSesodia <142791541+PrathamSesodia@users.noreply.github.com>
2024-01-31 02:00:32 -0800

    Update q1.c

--- Week-1/q1.c
+++ Week-1/q1.c
@@ -1,1 +1,31 @@
-oo
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,m;
+  cout<<"Enter the number of lists you want: ";
+  cin>>n;
+  while(n!=0)
+  {
+    cout<<"Enter the size of array: ";
+    cin>>m;
+    int a[m];
+    for(int i=0;i<m;i++)
+      cin>>a[i];
+    int k,count=0;
+    cout<<"Enter the number you want to search: ";
+    cin>>k;
+    for(int i=0;i<m;i++)
+    {
+      if(k==a[m])
+      { 
+        count=1;
+        cout<<"Number is present at: "<<i+1;
+        break;
+      }
+    }
+      if(count==0)
+        cout<<"Number is not present"<<endl;
+    n--;
+  }
+}
